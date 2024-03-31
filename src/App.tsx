cddea0197d5d931cbde360890597046410f05dca
Davide Barbagallo <barbang.davide@gmail.com>
2024-03-31 21:20:50 +0200

    feat: add color in tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { type FC } from 'react'
 const App: FC = () => {
   return (
     <div className="m-4">
-      <p>Hello World!</p>
+      <p className="text-grey-500">Hello World!</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: '#000000',
+      grey: {
+        100: '#F7F9FB',
+        200: '#EBEFF2',
+        300: '#E2E6EA',
+        400: '#CDD3D8',
+        500: '#242634'
+      },
+      'flat-grey': '#F8F8F8',
+      white: '#FFFFFF',
+      blue: '#4353FF',
+      green: '#11D075',
+      red: '#FF1640',
+      pink: '#FB51D6',
+      'light-pink': '#FF99E9'
+    },
     extend: {}
   },
   plugins: []
