2648fbe15d6ec1bf8c473e68871f464d9404fa3d
Davide Barbagallo <barbang.davide@gmail.com>
2024-04-05 17:35:53 +0200

    feat(Button): complete variants

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,7 +2,7 @@ import { type ButtonHTMLAttributes, type DetailedHTMLProps, type FC, useMemo } f
 import clsx from 'clsx'
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
-  variant?: 'outline' | 'text' | 'ghost'
+  variant?: 'outline' | 'filled' | 'ghost'
 }
 
 const Button: FC<ButtonProps> = ({ children, variant = 'outline', ...props }) => {
@@ -10,8 +10,8 @@ const Button: FC<ButtonProps> = ({ children, variant = 'outline', ...props }) =>
     return clsx(props.className, 'px-5 py-2 rounded-full', {
       'bg-white text-grey-500 border border-grey-400 hover:bg-grey-100 active:bg-grey-300 active:text-black':
         variant === 'outline',
-      'text-blue-500': variant === 'text',
-      'bg-transparent text-blue-500': variant === 'ghost'
+      'bg-black text-white hover:text-grey-200 active:text-grey-300': variant === 'filled',
+      'bg-grey-100 text-black hover:bg-grey-200 active:bg-grey-300': variant === 'ghost'
     })
   }, [props.className, variant])
 
