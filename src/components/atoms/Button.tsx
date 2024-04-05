04b3f482d1857febc71b434fa47c816f7f3bc12c
Davide Barbagallo <barbang.davide@gmail.com>
2024-04-05 18:49:41 +0200

    feat(Button): add size and rounded options

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,17 +3,23 @@ import clsx from 'clsx'
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   variant?: 'outline' | 'filled' | 'ghost'
+  size?: 'sm' | 'md' | 'lg'
+  rounded?: boolean
 }
 
-const Button: FC<ButtonProps> = ({ children, variant = 'outline', ...props }) => {
+const Button: FC<ButtonProps> = ({ children, variant = 'outline', size = 'sm', rounded = true, ...props }) => {
   const className = useMemo(() => {
-    return clsx(props.className, 'px-5 py-2 rounded-full', {
+    return clsx(props.className, {
       'bg-white text-grey-500 border border-grey-400 hover:bg-grey-100 active:bg-grey-300 active:text-black':
         variant === 'outline',
       'bg-black text-white hover:text-grey-200 active:text-grey-300': variant === 'filled',
-      'bg-grey-100 text-black hover:bg-grey-200 active:bg-grey-300': variant === 'ghost'
+      'bg-grey-100 text-black hover:bg-grey-200 active:bg-grey-300': variant === 'ghost',
+      'px-5 py-2 text-sm': size === 'sm',
+      'px-8 py-4 text-base': size === 'md',
+      'px-12 py-5 text-lg': size === 'lg',
+      'rounded-full': rounded
     })
-  }, [props.className, variant])
+  }, [props.className, variant, size, rounded])
 
   return (
     <button {...props} className={className}>
