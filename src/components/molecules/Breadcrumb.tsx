fe54987c3a352570a46608dc596b2ebd755f4916
Davide Barbagallo <barbang.davide@gmail.com>
2024-05-19 12:24:52 +0200

    fix(Breadcrumb): missing key prop

--- src/components/molecules/Breadcrumb.tsx
+++ src/components/molecules/Breadcrumb.tsx
@@ -1,4 +1,4 @@
-import { type FC, useMemo } from 'react'
+import { type FC, Fragment, useMemo } from 'react'
 import type { Directory } from 'src/lib/pst/types'
 import BreadcrumbItem from 'src/components/atoms/BreadcrumbItem'
 import BreadcrumbSeparator from 'src/components/atoms/BreadcrumbSeparator'
@@ -23,15 +23,10 @@ const Breadcrumb: FC<BreadcrumbProps> = (props) => {
   return (
     <div className="flex items-center gap-x-2">
       {items.map((item, index, array) => (
-        <>
-          <BreadcrumbItem
-            key={item.id}
-            text={item.name}
-            onClick={() => props.onNavigate(item)}
-            bold={index === array.length - 1}
-          />
+        <Fragment key={item.id}>
+          <BreadcrumbItem text={item.name} onClick={() => props.onNavigate(item)} bold={index === array.length - 1} />
           {index < array.length - 1 && <BreadcrumbSeparator />}
-        </>
+        </Fragment>
       ))}
     </div>
   )
